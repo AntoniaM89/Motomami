c9a247c7ed3c46a2557243271e23e9c18721c3c8
AntoniaM89 <an.marambio@duocuc.cl>
2023-11-12 22:14:22 -0300

    mapa
    se agrego un mapa, se espera cambiarlo pronto:D saludos amistad

--- src/app/tom-viaje/tom-viaje.page.ts
+++ src/app/tom-viaje/tom-viaje.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { StorageService } from '../servicios/storage.service';
 import { AtenticacionService } from '../servicios/autenticacion.service';
 import { Geolocation } from '@capacitor/geolocation';
+import * as L from 'leaflet';
 @Component({
   selector: 'app-tom-viaje',
   templateUrl: './tom-viaje.page.html',
@@ -9,20 +10,22 @@ import { Geolocation } from '@capacitor/geolocation';
 })
 export class TomViajePage implements OnInit {
   coordinates: any;
-  constructor(private storage: StorageService, private autenticacion: AtenticacionService) { }
+  //@ts-ignore
+  map: L.Map;
+  constructor(private storage: StorageService, private autenticacion: AtenticacionService, ) {}
   mostrarDestinos: { [key: string]: any } = [];
   async ngOnInit() {
     this.mostrarDestinos = this.storage.mostrarDestinos();
     console.log(this.mostrarDestinos);
     this.coordinates = await Geolocation.getCurrentPosition();
     console.log('Current position:', this.coordinates);
+    this.map = L.map('map').setView([this.coordinates.coords.latitude, this.coordinates.coords.longitude], 18);
+  
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(this.map);
     };
   }
 
-
-  
-  
-  
-  
 
 
