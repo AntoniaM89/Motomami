4c919e6d9d7bc5c97ff2db5eb16259228821ad49
AntoniaM89 <an.marambio@duocuc.cl>
2023-11-22 19:48:07 -0300

    aa

--- src/app/tom-viaje/tom-viaje.page.ts
+++ src/app/tom-viaje/tom-viaje.page.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { StorageService } from '../servicios/storage.service';
-import { AtenticacionService } from '../servicios/autenticacion.service';
-import { Geolocation } from '@capacitor/geolocation';
 import * as mapboxgl from 'mapbox-gl';
-
+import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-tom-viaje',
@@ -15,14 +13,9 @@ export class TomViajePage implements OnInit {
   //@ts-ignore
   map: mapboxgl.Map;
 
-  constructor(private storage: StorageService, private autenticacion: AtenticacionService) {}
-
-  mostrarDestinos: { [key: string]: any } = [];
+  constructor() {}
 
   async ngOnInit() {
-    this.mostrarDestinos = this.storage.mostrarDestinos();
-    console.log(this.mostrarDestinos);
-
     this.coordinates = await Geolocation.getCurrentPosition();
     console.log('Current position:', this.coordinates);
 
@@ -31,12 +24,22 @@ export class TomViajePage implements OnInit {
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v11',
         center: [this.coordinates.coords.longitude, this.coordinates.coords.latitude],
-        zoom: 16,
         accessToken: 'pk.eyJ1IjoiYW50b25pYTg5IiwiYSI6ImNsb3czM2hibjBlaXIycXFteml0MXZvNHAifQ.rv_0tetno62zXFX989Kzew',
+        zoom: 16,
       });
+
+      // Inicializar la biblioteca de direcciones
+      const directions = new MapboxDirections({
+        accessToken: mapboxgl.accessToken,
+        unit: 'metric',
+        profile: 'mapbox/driving-traffic',
+        controls: { instructions: true },
+      });
+
+      // Añadir control de direcciones al mapa
+      this.map.addControl(directions, 'top-left');
     } else {
       console.error('Error: No se pudo obtener la posición actual.');
     }
   }
-  
 }
