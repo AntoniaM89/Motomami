a0cb58ce6747d215a9802fe036ba4b10b4a062bc
Pansy <so.alfaro@duocuc.cl>
2023-11-25 01:20:03 -0300

    mas cosas deu

--- src/app/servicios/db.service.ts
+++ src/app/servicios/db.service.ts
@@ -11,45 +11,47 @@ import { Usuario, Viaje } from '../interfaces/idb';
 export class DbService {
   constructor(private http: HttpClient) {}
 
+  apiURL= 'http://localhost:3000'
+
   obtenerUsuarios(): Observable<Usuario[]> {
-    return this.http.get<Usuario[]>(`${environment.apiURL}/usuarios`).pipe(
+    return this.http.get<Usuario[]>(`${this.apiURL}/usuarios`).pipe(
       catchError(this.handleHttpError)
     );
   }
 
   crearUsuario(newUsuario: Usuario): Observable<Usuario> {
-    return this.http.post<Usuario>(`${environment.apiURL}/usuarios`, newUsuario).pipe(
+    return this.http.post<Usuario>(`${this.apiURL}/usuarios`, newUsuario).pipe(
       catchError(this.handleHttpError)
     );
   }
 
   obtenerUsuarioPorCorreo(correo: string): Observable<Usuario | null> {
-    return this.http.get<Usuario[]>(`${environment.apiURL}/usuarios`).pipe(
+    return this.http.get<Usuario[]>(`${this.apiURL}/usuarios`).pipe(
       catchError(this.handleHttpError),
       map((usuarios: Usuario[]) => usuarios.find((usuario) => usuario.correo === correo) || null)
     );
   }
 
   agregarViaje(nuevoViaje: Viaje): Observable<void> {
-    return this.http.post<void>(`${environment.apiURL}/viajes`, nuevoViaje).pipe(
+    return this.http.post<void>(`${this.apiURL}/viajes`, nuevoViaje).pipe(
       catchError(this.handleHttpError)
     );
   }
 
   obtenerViajes(): Observable<Viaje[]> {
-    return this.http.get<Viaje[]>(`${environment.apiURL}/viajes`).pipe(
+    return this.http.get<Viaje[]>(`${this.apiURL}/viajes`).pipe(
       catchError(this.handleHttpError)
     );
   }
 
   scheduleTrip(tripDetails: any): Observable<any> {
-    return this.http.post(`${environment.apiURL}/usuario`, tripDetails).pipe(
+    return this.http.post(`${this.apiURL}/usuario`, tripDetails).pipe(
       catchError(this.handleHttpError)
     );
   }
 
   getPassengerTrips(email: string): Observable<any> {
-    return this.http.get(`${environment.apiURL}/viajes?pasajeroCorreo=${email}`).pipe(
+    return this.http.get(`${this.apiURL}/viajes?pasajeroCorreo=${email}`).pipe(
       catchError(this.handleHttpError)
     );
   }
